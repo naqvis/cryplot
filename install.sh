12bbcc9c0c93365255a7ec7e6a67f338bbafff50
Jason Kridner <jkridner@beagleboard.org>
2024-11-14 17:59:24 -0500

    attempt direct clone for shards install

--- install.sh
+++ install.sh
@@ -6,9 +6,16 @@ cd $(dirname $0)
 
 git submodule update --init
 
-if [ ! -d "deps/gnuplot-palettes" ]
+if [ ! -e "deps/gnuplot-palettes/accent.pal" ]
 then
     echo "missing directory 'deps/gnuplot-palettes'. Seems  git submodule didn't work"
+    echo "trying direct clone of latest..."
+    git clone https://github.com/naqvis/gnuplot-palettes deps/gnuplot-palettes
+fi
+
+if [ ! -e "deps/gnuplot-palettes/accent.pal" ]
+then
+    echo "git clone didn't work either"
     exit 2
 fi
 
